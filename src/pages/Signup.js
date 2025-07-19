c9c51adedf88b3f4759d81297ef12d39ad363d2a
Anurag224132 <1pandeyanurag1@gmail.com>
2025-07-19 13:51:22 +0530

    commit

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -15,7 +15,7 @@ const Signup = () => {
   const navigate = useNavigate();
   const [showPassword, setShowPassword] = useState(false);
 
-  const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000';
+  const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;// || 'http://localhost:5000';
 
   const handleRegister = async (e) => {
     e.preventDefault();
